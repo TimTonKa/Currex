2dbd92c106a99e7f584e4ed393abfcce08c19d64
timzheng <timzheng@truetel.com>
2025-06-04 10:28:57 +0800

    add L10n file

---
+++ Currex/res/L10n.swift
@@ -0,0 +1,16 @@
+//
+//  L10n.swift
+//  Currex
+//
+//  Created by Tim Tseng on 2025/6/4.
+//
+
+import Foundation
+
+import SwiftUI
+
+struct L10n {
+    static let search = LocalizedStringKey("search_country_or_currency")
+    static let selectCurrency = LocalizedStringKey("select_currency")
+    // 持續加入更多 key
+}
